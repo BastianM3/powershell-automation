cfb47dcdf5d849846a86b699a640cdbd0265e5f9
BastianM3 <mbastian@labtechsoftware.com>
2016-02-03 16:35:00 -0500

    Pushing a snippet of code that will download and install the "dark matrix" theme preset I created.

---
+++ PowerShell Studio Themes/Install SAPIEN PowerShell Studio Theme.ps1
@@ -0,0 +1,46 @@
+﻿function Download-FileFromURL ($url, $saveAs)
+{
+	$webclient = New-Object System.net.WebClient;
+	$webclient.DownloadFile($url, $saveAs);
+	
+	if (Test-Path $saveAs)
+	{
+		return $true;
+	}
+	else
+	{
+		return $false;
+	}
+	
+	
+}
+
+$URL = "https://gitlab.com/ByteEater/powershell-fun/blob/master/PowerShell%20Studio%20Themes/Dark%20Matrix.preset";
+$SapienAppDataPath = "$env:AppData\Roaming\SAPIEN\PowerShell Studio 2015\Editor Presets";
+
+if (-not (test-path $SapienAppDataPath))
+{
+    New-Item -ItemType Directory -Path $SapienAppDataPath -ev newitemError;
+}
+
+if ($newitemError)
+{
+    Write-Error "Failed to create Editor Presets directory in: $SapienAppDataPath";
+    return;
+}
+else
+{
+    $Downloadresult = Download-FileFromURL $URL "$SapienAppDataPath\DarkMatrix.preset"
+}
+
+if ($Downloadresult)
+{
+    Write-Host "Successfully installed PowerShell Studio Preset!" -ForegroundColor Green
+}
+else
+{
+    Write-Error "Failed to install PowerShell Studio Preset!";
+}
+
+Read-Host "Hit any key to exit.";
+exit;
