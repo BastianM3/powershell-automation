5aaa6ff2e5d7daa96d1b7c84652a9b649dbdd1f9
MBastian <BastianM3@gmail.com>
2016-03-23 13:49:01 -0400

    Created a script that examplifies the declaration of classes in PowerShell 5.0 as well as how to instatiate objects using them.

---
+++ PowerShell 5.0/PowerShell 5.0 - Classes in PowerShell.ps1
@@ -0,0 +1,39 @@
+﻿#Requires -version 5.0
+
+class Vehicle 
+{
+    [string]$Manufacturer
+    [string]$ModelName
+    [int]$Year
+
+    Vehicle($Manufacturer, $ModelName, $year )
+    {
+        $this.Manufacturer = $Manufacturer
+        $this.ModelName    = $ModelName
+        $this.Year         = $year
+    }
+
+    hidden [int] $speed
+
+    [void] IncreaseThrottle()
+    {
+        $this.Speed++;
+    }
+
+    [int] GetSpeed()
+    {
+        return $this.Speed;
+    }
+}
+
+# Instantiate an object using the constructor defined in the PowerShell 5.0 class above
+$myVehicle = new-object Vehicle("Ford","Edge",2005);
+
+for($i =0; $i -lt 50; $i ++)
+{
+    $myVehicle.IncreaseThrottle();
+}
+
+# Call method that returns the current value of the hidden $speed member.
+$myVehicle.GetSpeed();
+
